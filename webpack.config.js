c19c764f5826d2a89c666b228edd305705856b6e
refarde <refarde@gmail.com>
2018-01-24 21:52:32 +0900

    slow~ slow~

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,71 @@
-// webpack.config.js
+
 var path = require( "path" ),
 	webpack = require( "webpack" ),
 	HtmlWebpackPlugin = require( "html-webpack-plugin" ),
-	ExtractTextPlugin = require("extract-text-webpack-plugin");
+	ExtractTextPlugin = require( "extract-text-webpack-plugin" ),
+	ReplaceBundlePlugin = require( "replace-bundle-webpack-plugin" ),
+	_args = require( "yargs" ).argv,
+	_package = require( "./package.json" ),
+	_isProduction = _args.p || process.env.NODE_ENV === "production";
 
-module.exports = {
-	//devtool: "cheap-eval-source-map",
+module.exports = ( function() {
+	"use strict";
 
-	entry: [
-		"./src/latteart"
-	],
-
-	output: {
-		path: path.join( __dirname, "dist" ),
-		filename: "latteart.js"
-	},
-
-	plugins: [
-		// new webpack.optimize.UglifyJsPlugin({
-		// 	compressor: {
-		// 		warnings: true
-		// 	}
-		// }),
-		// new webpack.optimize.OccurrenceOrderPlugin(),
-		new HtmlWebpackPlugin({
+	var plugins = [
+		new HtmlWebpackPlugin( {
 			template: "./src/tpls/index.html"
-		}),
-		new ExtractTextPlugin({
+		} ),
+		new ExtractTextPlugin( {
 			filename: "style.css",
 			disable: false,
 			allChunks: true
-		}),
-	],
+		} ),
+		new ReplaceBundlePlugin( [
+			{
+				partten: /@@EDITOR_VERSION@@/g,
+				replacement: function() {
+					return _args.version || _package.version;
+				}
+			}, {
+				partten: /@@EDITOR_EDITION@@/g,
+				replacement: function() {
+					return _args.edition || "dev";
+				}
+			}
+		] )
+	];
 
-	module: {
-		loaders: [{
-			test: /\.less$/,
-			use: ExtractTextPlugin.extract({
-				fallback: "style-loader",
-				use: [ "css-loader", "less-loader" ]
-			})
-		}]
+	if ( _isProduction ) {
+		plugins = [
+				new webpack.optimize.UglifyJsPlugin( {
+					compressor: {
+						warnings: true
+					}
+				} ),
+				new webpack.optimize.OccurrenceOrderPlugin()
+			].concat( plugins );
 	}
-};
+
+	return {
+		entry: [
+			"./src/latteart"
+		],
+
+		output: {
+			path: path.join( __dirname, "dist" ),
+			filename: "latteart.js"
+		},
+
+		plugins: plugins,
+
+		module: {
+			loaders: [ {
+				test: /\.less$/,
+				use: ExtractTextPlugin.extract( {
+					fallback: "style-loader",
+					use: [ "css-loader", "less-loader" ]
+				} )
+			} ]
+		}
+	};
+} )();
